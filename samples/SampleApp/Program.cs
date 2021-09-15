756a128636a689414036d6521c8c16de8679c9d1
Tho Ho <code@tho.email>
2021-09-15 18:57:03 +0800

    Format code

--- samples/SampleApp/Program.cs
+++ samples/SampleApp/Program.cs
@@ -8,13 +8,13 @@ namespace SampleApp
         public static void Main(string[] args)
         {
             var appInfo = ApplicationInfo.Initialize(null);
-            Console.WriteLine($"AppTitle:{appInfo.Title}");
+            Console.WriteLine($"AppTitle: {appInfo.Title}");
             Console.WriteLine($"Version:{appInfo.InformationalVersion} BuildTime:{appInfo.BuildTimestampLocal}; Framework:{appInfo.FrameworkName}");
 
             Console.WriteLine("");
 
             var asm = new AssemblyInfo(typeof(AssemblyInfo).Assembly);
-            Console.WriteLine($"AssemblyTitle:{asm.Title}");
+            Console.WriteLine($"AssemblyTitle: {asm.Title}");
             Console.WriteLine($"Version:{asm.InformationalVersion} BuildTime:{asm.BuildTimestampLocal}; Framework:{asm.FrameworkName}");
         }
     }
