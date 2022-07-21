c0b002ec5b91352185d2f8bb666ecf59d1dcbd3a
Tho Ho <code@tho.email>
2022-07-21 14:00:50 +0800

    Implement C# v10.0, ImplicitUsings and Nullable

--- NetLah.Abstractions/Diagnostics/ApplicationInfo.cs
+++ NetLah.Abstractions/Diagnostics/ApplicationInfo.cs
@@ -1,62 +1,60 @@
-﻿using System;
-using System.Reflection;
+﻿using System.Reflection;
 
-namespace NetLah.Diagnostics
+namespace NetLah.Diagnostics;
+
+/// <summary>
+/// ApplicationInfo for entry assembly
+/// </summary>
+/// <example>
+/// Initialize single instance of ApplicationInfo first
+/// <code>
+/// var appInfo = ApplicationInfo.Initialize(Assembly.GetEntryAssembly());
+/// </code>
+/// </example>
+/// <returns></returns>
+public sealed class ApplicationInfo : IAssemblyInfo
 {
+    private static readonly object _staticSyncRoot = new();
+
+    public static ApplicationInfo? Instance { get; private set; }
+
     /// <summary>
-    /// ApplicationInfo for entry assembly
+    /// Initialize single instance of ApplicationInfo
     /// </summary>
-    /// <example>
-    /// Initialize single instance of ApplicationInfo first
-    /// <code>
-    /// var appInfo = ApplicationInfo.Initialize(Assembly.GetEntryAssembly());
-    /// </code>
-    /// </example>
+    /// <param name="assembly">Provide null for Assembly.GetEntryAssembly()</param>
     /// <returns></returns>
-    public sealed class ApplicationInfo : IAssemblyInfo
+    public static ApplicationInfo Initialize(Assembly? assembly)
     {
-        private static readonly object _staticSyncRoot = new();
-
-        public static ApplicationInfo Instance { get; private set; }
-
-        /// <summary>
-        /// Initialize single instance of ApplicationInfo
-        /// </summary>
-        /// <param name="assembly">Provide null for Assembly.GetEntryAssembly()</param>
-        /// <returns></returns>
-        public static ApplicationInfo Initialize(Assembly assembly)
+        lock (_staticSyncRoot)
         {
-            lock (_staticSyncRoot)
+            if (Instance is { } instance)
             {
-                if (Instance is { } instance)
-                {
-                    throw new InvalidOperationException($"ApplicationInfo is already initialized with assembly: {instance.AssemblyInfo.Name.FullName}");
-                }
-                Instance = new ApplicationInfo(assembly ?? System.Reflection.Assembly.GetEntryAssembly());
+                throw new InvalidOperationException($"ApplicationInfo is already initialized with assembly: {instance.AssemblyInfo.Name.FullName}");
             }
-            return Instance;
+            Instance = new ApplicationInfo(assembly ?? Assembly.GetEntryAssembly());
         }
-
-        private ApplicationInfo(Assembly assembly) => AssemblyInfo = new AssemblyInfo(assembly);
-
-        public IAssemblyInfo AssemblyInfo { get; }
-
-        public string InformationalVersion => AssemblyInfo.InformationalVersion;
-
-        public string Product => AssemblyInfo.Product;
-
-        public string Title => AssemblyInfo.Title;
-
-        public string Description => AssemblyInfo.Description;
-
-        public AssemblyName Name => AssemblyInfo.Name;
-
-        public string ImageRuntimeVersion => AssemblyInfo.ImageRuntimeVersion;
-
-        public string FrameworkName => AssemblyInfo.FrameworkName;
-
-        public DateTimeOffset? BuildDate => AssemblyInfo.BuildDate;
-
-        public string BuildTimestampLocal => AssemblyInfo.BuildTimestampLocal;
+        return Instance;
     }
+
+    private ApplicationInfo(Assembly assembly) => AssemblyInfo = new AssemblyInfo(assembly);
+
+    public IAssemblyInfo AssemblyInfo { get; }
+
+    public string InformationalVersion => AssemblyInfo.InformationalVersion;
+
+    public string Product => AssemblyInfo.Product;
+
+    public string Title => AssemblyInfo.Title;
+
+    public string Description => AssemblyInfo.Description;
+
+    public AssemblyName Name => AssemblyInfo.Name;
+
+    public string ImageRuntimeVersion => AssemblyInfo.ImageRuntimeVersion;
+
+    public string FrameworkName => AssemblyInfo.FrameworkName;
+
+    public DateTimeOffset? BuildDate => AssemblyInfo.BuildDate;
+
+    public string BuildTimestampLocal => AssemblyInfo.BuildTimestampLocal;
 }
--- NetLah.Abstractions/Diagnostics/IAssemblyInfo.cs
+++ NetLah.Abstractions/Diagnostics/IAssemblyInfo.cs
@@ -1,20 +1,18 @@
-﻿using System;
-using System.Reflection;
+﻿using System.Reflection;
 
-namespace NetLah.Diagnostics
+namespace NetLah.Diagnostics;
+
+public interface IAssemblyInfo
 {
-    public interface IAssemblyInfo
-    {
-        string InformationalVersion { get; }
-        string Product { get; }
-        string Title { get; }
-        string Description { get; }
-        AssemblyName Name { get; }
-        string ImageRuntimeVersion { get; }
-        string FrameworkName { get; }
-        DateTimeOffset? BuildDate { get; }
+    string InformationalVersion { get; }
+    string Product { get; }
+    string Title { get; }
+    string Description { get; }
+    AssemblyName Name { get; }
+    string ImageRuntimeVersion { get; }
+    string FrameworkName { get; }
+    DateTimeOffset? BuildDate { get; }
 
-        // format: "1970-01-01T23:59:59+08:00"
-        string BuildTimestampLocal { get; }
-    }
+    // format: "1970-01-01T23:59:59+08:00"
+    string BuildTimestampLocal { get; }
 }
