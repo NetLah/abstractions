ea939d47fa3c22cabc3ccf6fcc694577fa53af55
Tho Ho <code@tho.email>
2022-10-28 23:50:17 +0800

    Update sampleApp show StartTime and Uptime

--- samples/SampleApp/Program.cs
+++ samples/SampleApp/Program.cs
@@ -3,6 +3,7 @@
 var appInfo = ApplicationInfo.Initialize(null);
 Console.WriteLine($"AppTitle: {appInfo.Title}");
 Console.WriteLine($"Version:{appInfo.InformationalVersion} BuildTime:{appInfo.BuildTimestampLocal}; Framework:{appInfo.FrameworkName}");
+Console.WriteLine($"StartTime:{appInfo.StartTime} Uptime:{appInfo.Uptime}");
 
 Console.WriteLine("");
 
