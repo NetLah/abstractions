f3c2becf558d8426502ac86383b9dfd7ca2e235c
Tho Ho <code@tho.email>
2022-10-28 23:17:21 +0800

    Add ValueStopwatch

---
+++ NetLah.Abstractions.Test/ValueStopwatchTest.cs
@@ -0,0 +1,34 @@
+﻿using NetLah.Diagnostics;
+using Xunit;
+
+namespace NetLah.Abstractions.Test;
+
+public class ValueStopwatchTest
+{
+    [Fact]
+    public void IsActiveIsFalseForDefaultValueStopwatch()
+    {
+        Assert.False(default(ValueStopwatch).IsActive);
+    }
+
+    [Fact]
+    public void IsActiveIsTrueWhenValueStopwatchStartedWithStartNew()
+    {
+        Assert.True(ValueStopwatch.StartNew().IsActive);
+    }
+
+    [Fact]
+    public void GetElapsedTimeThrowsIfValueStopwatchIsDefaultValue()
+    {
+        var stopwatch = default(ValueStopwatch);
+        Assert.Throws<InvalidOperationException>(() => stopwatch.GetElapsedTime());
+    }
+
+    [Fact]
+    public async Task GetElapsedTimeReturnsTimeElapsedSinceStart()
+    {
+        var stopwatch = ValueStopwatch.StartNew();
+        await Task.Delay(200);
+        Assert.True(stopwatch.GetElapsedTime().TotalMilliseconds > 0);
+    }
+}
---
+++ NetLah.Abstractions/Diagnostics/ValueStopwatch.cs
@@ -0,0 +1,38 @@
+﻿using System.Diagnostics;
+
+namespace NetLah.Diagnostics;
+
+/// <summary>
+/// Referrence from https://github.com/dotnet/runtime/blob/c9f7f7389/src/libraries/Microsoft.Extensions.Http/src/ValueStopwatch.cs<br/>
+/// And from https://www.meziantou.net/how-to-measure-elapsed-time-without-allocating-a-stopwatch.htm
+/// </summary>
+public struct ValueStopwatch
+{
+    private static readonly double TimestampToTicks = TimeSpan.TicksPerSecond / (double)Stopwatch.Frequency;
+
+    private readonly long _startTimestamp;
+
+    public bool IsActive => _startTimestamp != 0;
+
+    private ValueStopwatch(long startTimestamp)
+    {
+        _startTimestamp = startTimestamp;
+    }
+
+    public static ValueStopwatch StartNew() => new(Stopwatch.GetTimestamp());
+
+    public TimeSpan GetElapsedTime()
+    {
+        // Start timestamp can't be zero in an initialized ValueStopwatch. It would have to be literally the first thing executed when the machine boots to be 0.
+        // So it being 0 is a clear indication of default(ValueStopwatch)
+        if (!IsActive)
+        {
+            throw new InvalidOperationException("An uninitialized, or 'default', ValueStopwatch cannot be used to get elapsed time.");
+        }
+
+        long end = Stopwatch.GetTimestamp();
+        long timestampDelta = end - _startTimestamp;
+        long ticks = (long)(TimestampToTicks * timestampDelta);
+        return new TimeSpan(ticks);
+    }
+}
