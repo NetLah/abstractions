ceb8746f87c75922ca7146dc0ec22cc0f840ba6b
Tho Ho <code@tho.email>
2023-11-24 16:24:09 +0800

    Readonly struct

--- NetLah.Abstractions/Diagnostics/ValueStopwatch.cs
+++ NetLah.Abstractions/Diagnostics/ValueStopwatch.cs
@@ -6,20 +6,23 @@ namespace NetLah.Diagnostics;
 /// Referrence from https://github.com/dotnet/runtime/blob/c9f7f7389/src/libraries/Microsoft.Extensions.Http/src/ValueStopwatch.cs<br/>
 /// And from https://www.meziantou.net/how-to-measure-elapsed-time-without-allocating-a-stopwatch.htm
 /// </summary>
-public struct ValueStopwatch
+public readonly struct ValueStopwatch
 {
     private static readonly double TimestampToTicks = TimeSpan.TicksPerSecond / (double)Stopwatch.Frequency;
 
     private readonly long _startTimestamp;
 
-    public bool IsActive => _startTimestamp != 0;
+    public readonly bool IsActive => _startTimestamp != 0;
 
     private ValueStopwatch(long startTimestamp)
     {
         _startTimestamp = startTimestamp;
     }
 
-    public static ValueStopwatch StartNew() => new(Stopwatch.GetTimestamp());
+    public static ValueStopwatch StartNew()
+    {
+        return new(Stopwatch.GetTimestamp());
+    }
 
     public TimeSpan GetElapsedTime()
     {
@@ -30,9 +33,9 @@ public struct ValueStopwatch
             throw new InvalidOperationException("An uninitialized, or 'default', ValueStopwatch cannot be used to get elapsed time.");
         }
 
-        long end = Stopwatch.GetTimestamp();
-        long timestampDelta = end - _startTimestamp;
-        long ticks = (long)(TimestampToTicks * timestampDelta);
+        var end = Stopwatch.GetTimestamp();
+        var timestampDelta = end - _startTimestamp;
+        var ticks = (long)(TimestampToTicks * timestampDelta);
         return new TimeSpan(ticks);
     }
 }
